ebbb23e873ea29bf8f001a1d12b9f9e289532a96
DamirV <32214748+DamirV@users.noreply.github.com>
2020-07-01 18:09:05 +0300

    Add files via upload

---
+++ Ode/Ode/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ode
+{
+    public partial class Form1 : Form
+    {
+        double[] y;
+        double[] x;
+        double a;
+        double b;
+        double h;
+        int n;
+        int series;
+
+        public Form1()
+        {
+            InitializeComponent();
+            y = new double[n];
+            x = new double[n];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                chart1.Series.Clear();
+                Ode o = new Ode();
+                o.setab(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text));
+                a = Convert.ToDouble(textBox3.Text);
+                b = Convert.ToDouble(textBox4.Text);
+                h = Convert.ToDouble(textBox5.Text);
+                series = Convert.ToInt32(textBox6.Text);
+
+                n = (int)((b - a) / h);
+
+                for (int j = 0; j < series; ++j)
+                {
+                    chart1.Series.Add("series" + j);
+                    chart1.Series[j].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+                    chart1.Series[j].Color = Color.Blue;
+                    chart1.Series[j].BorderWidth = 1;
+
+                    y = o.ode(a, j - (int)(series / 2), a, b, h);
+
+                    for (int i = 0; i < n; ++i)
+                    {
+                        chart1.Series[j].Points.AddXY(a + h * i, y[i]);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+    }
+}
---
+++ Ode/Ode/Ode.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ode
+{
+    class Ode
+    {
+        private double a = 1;
+        private double b = 0;
+        double F(double x, double y)
+        {
+            //return a * x * x * y + b;
+            return x * x - 2 * y;
+        }
+        public void setab(double _a, double _b) {
+            a = _a;
+            b = _b;
+        }
+
+       public double[] ode(double x0, double y0, double a, double b, double h)
+        {
+            int n = (int)((b - a) / h);
+            double[] X = new double[n];
+            double[] K1 = new double[n];
+            double[] K2 = new double[n];
+            double[] K3 = new double[n];
+            double[] K4 = new double[n];
+            double[] Y = new double[n];
+
+            X[0] = x0; Y[0] = y0;
+            for (int i = 1; i < n; i++)
+            {
+                X[i] = a + i * h;
+                K1[i] = h * F(X[i - 1], Y[i - 1]);
+                K2[i] = h * F(X[i - 1] + h / 2.0, Y[i - 1] + K1[i] / 2.0);
+                K3[i] = h * F(X[i - 1] + h / 2, Y[i - 1] + K2[i] / 2);
+                K4[i] = h * F(X[i - 1] + h, Y[i - 1] + K3[i]);
+                Y[i] = Y[i - 1] + (K1[i] + 2 * K2[i] + 2 * K3[i] + K4[i]) / 6;
+            }
+            return Y;
+        }
+
+        double F1(double x, double y, double t)
+        {
+            return y;
+        }
+
+        double F2(double x, double y, double t)
+        {
+            return -x;
+        }
+
+        public void ode(double x0, double y0, double t0, double start, double stop, double h)
+        {
+            int n = (int)((stop - start) / h);
+            double[] X = new double[n];
+            double[] T = new double[n];
+            double[] K1 = new double[n];
+            double[] K2 = new double[n];
+            double[] K3 = new double[n];
+            double[] K4 = new double[n];
+            double[] M1 = new double[n];
+            double[] M2 = new double[n];
+            double[] M3 = new double[n];
+            double[] M4 = new double[n];
+            double[] Y = new double[n];
+
+            X[0] = x0; Y[0] = y0; T[0] = t0;
+        }
+
+    }
+}
